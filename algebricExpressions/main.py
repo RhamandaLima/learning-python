7d28087e0ac8be2d4c18499e4aa486d20b0848af
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-09-28 21:02:28 -0300

    feat: adding algebric expression exercises

---
+++ algebricExpressions/main.py
@@ -0,0 +1,15 @@
+somatorio = 1 + 2 + 3 + 4 + 5
+
+media = (23 + 19 + 31) / 3
+
+vezes = 403 // 73
+
+sobra = 403 % 73
+
+potencia = 2**10
+
+valorAbsoluto = abs(54 - 57)
+
+menorValor = min(34, 29, 31)
+
+print(somatorio, media, vezes, sobra, potencia, valorAbsoluto, menorValor)
