c33d7ac606f8a8a8d42eafe9de8cbdf796158051
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-07 17:53:50 -0300

    feat: adding teacher's version of exercise 03 from class 03

--- classThree/exerciseThreeClassThree/main.py
+++ classThree/exerciseThreeClassThree/main.py
@@ -5,29 +5,24 @@
 print('CONSUMO ELÉTRICO')
 
 tipo = input('Qual o tipo de instalação? \nDigite R para residências, I para indústrias e C para comércios: ')
-if tipo == 'R' or tipo == 'C' or tipo == 'I':
-    qtdConsumo = float(input('Qual a quantidade de kWr consumida? '))
+qtdConsumo = float(input('Qual a quantidade de kWr consumida? '))
 
 if tipo == 'R':
     if qtdConsumo <= 500:
-        calculo = round((qtdConsumo * 0.40), 2)
-        print('O consumo foi de {} para o tipo {}. \nE o valor a ser pago é: {}'.format(qtdConsumo, tipo, calculo))
+        preco = 0.40
     else:
-        calculo = round((qtdConsumo * 0.65), 2)
-        print('O consumo foi de {} para o tipo {}. \nE o valor a ser pago é: {}'.format(qtdConsumo, tipo, calculo))
+        preco = 0.65
 elif tipo == 'C':
     if qtdConsumo <= 1000:
-        calculo = round((qtdConsumo * 0.55), 2)
-        print('O consumo foi de {} para o tipo {}. \nE o valor a ser pago é: {}'.format(qtdConsumo, tipo, calculo))
+        preco = 0.55
     else:
-        calculo = round((qtdConsumo * 0.60), 2)
-        print('O consumo foi de {} para o tipo {}. \nE o valor a ser pago é: {}'.format(qtdConsumo, tipo, calculo))
+        preco = 0.60
 elif tipo == 'I':
     if qtdConsumo <= 5000:
-        calculo = round((qtdConsumo * 0.55), 2)
-        print('O consumo foi de {} para o tipo {}. \nE o valor a ser pago é: {}'.format(qtdConsumo, tipo, calculo))
+        preco = 0.55
     else:
-        calculo = round((qtdConsumo * 0.60), 2)
-        print('O consumo foi de {} para o tipo {}. \nE o valor a ser pago é: {}'.format(qtdConsumo, tipo, calculo))
+        preco = 0.60
 else:
     print('Instalação inválida')
+
+print('O consumo foi de {} para o tipo {}. \nE o valor a ser pago é: {}'.format(qtdConsumo, tipo, qtdConsumo * preco))
