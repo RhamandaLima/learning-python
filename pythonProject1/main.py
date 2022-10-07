df0b64609da8b66203fe2a479bb77f57bd746fd2
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-07 13:15:51 -0300

    feat: adding booleand expressions exercise

---
+++ pythonProject1/main.py
@@ -0,0 +1,17 @@
+# Escreva as seguintes expressões booleanas em linguagem Python:
+
+# a) O somatório de 2 com 2 é menor do que 4
+
+print(2 + 2 < 4)
+
+# b) O valor 7 // 3 é igual a 1 + 1
+
+print(7 // 3 == 1 + 1)
+
+# c) A soma de 3 elevado ao quadrado com 4 elevado ao quadrado é igual a 25
+
+print(3**2 + 4**2 == 25)
+
+# d) A soma de 2, 4 e 6 é maior do que 12
+
+print(2 + 4 + 6 > 12)
