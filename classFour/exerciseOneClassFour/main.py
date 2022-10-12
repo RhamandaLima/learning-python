94dcf2b2887042c4049f7a7b82500d69dda0dc3a
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-11 21:38:23 -0300

    feat: adding refactored version of exercise 01 from class 04

--- classFour/exerciseOneClassFour/main.py
+++ classFour/exerciseOneClassFour/main.py
@@ -7,32 +7,31 @@ print('CALCULADORA')
 print('Adição (+), Subtração (-), Multiplicação(*) e Divisão (/).')
 print('Pressione s para sair')
 
-operador = input('Qual operação deseja realizar? ')
-
-if operador == '+' or operador == '-' or operador == '*' or operador == '/':
-    valorA = int(input('Insira o primeiro valor: '))
-    valorB = int(input('Insira o segundo valor: '))
-
-while operador != 's':
-    if operador == '+':
-        soma = valorA + valorB
-        print('O resultado é: {} + {} = {}'.format(valorA, valorB, soma))
-    elif operador == '-':
-        subtracao = valorA - valorB
-        print('O resultado é: {} - {} = {}'.format(valorA, valorB, subtracao))
-    elif operador == '*':
-        multiplicacao = valorA * valorB
-        print('O resultado é: {} * {} = {}'.format(valorA, valorB, multiplicacao))
-    elif operador == '/':
-        divisao = valorA / valorB
-        print('O resultado é: {} / {} = {}'.format(valorA, valorB, divisao))
-    else:
-        print('Operação inválida')
-
+while True:
     operador = input('Qual operação deseja realizar? ')
-
     if operador == '+' or operador == '-' or operador == '*' or operador == '/':
         valorA = int(input('Insira o primeiro valor: '))
         valorB = int(input('Insira o segundo valor: '))
+
+    if operador == '+':
+        soma = valorA + valorB
+        print('O resultado é: {} + {} = {}'.format(valorA, valorB, soma))
+        continue
+    elif operador == '-':
+        subtracao = valorA - valorB
+        print('O resultado é: {} - {} = {}'.format(valorA, valorB, subtracao))
+        continue
+    elif operador == '*':
+        multiplicacao = valorA * valorB
+        print('O resultado é: {} * {} = {}'.format(valorA, valorB, multiplicacao))
+        continue
+    elif operador == '/':
+        divisao = valorA / valorB
+        print('O resultado é: {} / {} = {}'.format(valorA, valorB, divisao))
+        continue
+    elif operador == 's':
+        break
+    else:
+        print('Operação inválida')
 
 print('Encerrando o programa...')
