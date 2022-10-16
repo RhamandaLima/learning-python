6f1fd3a9f8ecc37a18e474bb263178fa9fbeb040
Rhamanda Lima <cathyannarhamanda@gmail.com>
2022-10-15 21:12:18 -0300

    feat: adding docstring example

---
+++ docstrings/main.py
@@ -0,0 +1,13 @@
+def soma(x=0, y=0, z=0):
+    """
+    Retorna o somatóio de até 3 valores números quaisquer.
+    Todos os parâmetros são opcionais.
+
+    :param x: valor número (opcional)
+    :param y: valor número (opcional)
+    :param z: valor número (opcional)
+    """
+    return x+y+z
+
+print(soma(3,2))
+help(soma)
